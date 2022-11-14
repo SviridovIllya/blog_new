1ccc35934de9f7bd78a87f8fb268e7cc0959ff42
SviridovIllya <ingrimn@gmail.com>
2022-11-14 23:42:43 +0200

    Initial commit

---
+++ laravel/resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,44 @@
+@extends('admin.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Редактировать категорию</h1>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-12">
+                        <form action="{{route('admin.category.update',$category->id)}}" method="POST" class="col-2">
+                            @csrf
+                            @method('PATCH')
+                            <div class="form-group">
+                                <input type="text"  class="form-control " name="title"  placeholder="Название категории" value="{{$category->title}}">
+                                @error('title')
+                                <div class="text-danger">Пустое поле </div>
+                                @enderror
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Обновить">
+                        </form>
+
+                    </div>
+
+                </div>
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
---
+++ laravel/resources/views/admin/tag/show.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.layouts.main')
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{$tag->title}}</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+
+                    <div class="card">
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <tbody>
+                                <tr>
+                                    <td>Id</td>
+                                    <td>Название</td>
+
+                                </tr>
+                                <tr>
+                                    <td>{{$tag->id}}</td>
+                                    <td>{{$tag->title}}</td>
+
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
